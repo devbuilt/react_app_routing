158237b7e630a420c3fdb6416b0ca5fd2655804e
Devbuilt <leo@devbuiltlabs.com>
2019-10-30 23:10:45 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Nav from './components/Nav/Nav';
+import Main from './components/Main/Main';
+import About from './components/About/About';
+// import Error from './components/Error/Error';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+   
+        <div>
+         <BrowserRouter>
+        <div>
+         <Nav/>
+        <Switch>
+        <Route path="/" component={Main} exact />
+        <Route path="/about" component={About} exact/>
+          {/* <Route component={Error} /> */}
+         </Switch>
+         </div>
+         </BrowserRouter>
+         </div>
+    
+    );
+  }
 }
 
 export default App;
